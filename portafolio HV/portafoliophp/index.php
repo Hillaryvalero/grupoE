0a5ad29d973f32c8d112882f6fa4fde52671e18d
Hillaryvalero <89660775+Hillaryvalero@users.noreply.github.com>
2021-10-11 15:11:50 -0500

    Add files via upload

---
+++ portafolio HV/portafoliophp/index.php
@@ -0,0 +1,259 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Portafolio Personal</title>
+<meta http-equiv="X-UA-Compatible" content="IE-edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="shortcut icon" href="images/icon.png">
+<link rel="stylesheet" type="text/css" href="css/style.css"/>
+
+
+</head>
+
+<body>
+	
+	<section id="main">
+	
+	<nav>
+	
+	<a href="#" class="logo">HV</a>
+
+	<div class="toggle"></div>
+	<ul class="menu">
+	<li class="active"><a href="#main" >Inicio</a></li>	
+	<li><a href="#about">Sobre mí</a></li>	
+	<li><a href="#services">Proyectos</a></li>	
+	<li><a href="#portfolio">PortaFolio</a></li>		
+	<li><a href="#contact-form">Contactame</a></li>	
+	</ul>
+
+	</div>
+
+	</nav>
+	
+	<div class="name">
+	
+	<p>Hola</p>
+	
+	<h1>Soy <font color="#AF3669">Hillary</font> Valero</h1>
+	
+	<p class="details">Soy desarrolladora de Front-End, 
+		Me encuentro estudiando analisis y desarrollo en sistemas de informacion Les presento 
+	mi portafolio en el cual dare a conocer unos proyectos que he podido realizar a lo largo de mi carrera como estudiante de 
+	ADSI.
+</p>
+	
+	<a href="#" class="cv-btn">Mi portafolio</a>
+	</div>
+		
+	<div class="black-line" ></div>
+	
+	<div class="social">
+	<a href="#"><i class="fab fa-facebook-f"></i></a>	
+	<a href="#"><i class="fab fa-twitter"></i></a>
+	<a href="#"><i class="fab fa-instagram"></i></a>
+	<a href="#"><i class="fab fa-youtube"></i></a>	
+	</div>
+	
+	</section>
+	<section id="about">
+	<div class="about-text">
+
+	<h1><font color="#AF3669">Sobre mí</h1>
+	<h2> Front-End Developer</h2>
+	<p>Disfruto realizar el diseño  de paginas web frontend es la parte
+		del desarrollo web que se dedica a la parte frontal de un sitio
+		web, en pocas palabras del diseño de un sitio web, desde la estructura 
+		del sitio hasta los estilos como colores, fondos, tamaños hasta llegar
+		a las animaciones y efectos..</p>
+	<button>Ver mas detalles</button>
+	</div>
+	
+	<div class="about-model">
+	<img src="images/about-model.jpg" alt="model" width="800px" height="800px">	
+	</div>
+	
+	
+	</section>
+	
+	<section id="services">
+	
+	<div class="s-heading">
+	<h1> <font color="#AF3669">Proyectos</h1>
+	<p>Estos son algunos de los Proyectos que he realizado</p>
+	</div>
+	
+	<div class="b-container">
+	
+	<div class="s-box">
+
+	<div class="s-b-img">
+		
+		<div class="s-type">Encicloplay</div>
+	
+		<img src="images/s1.png">
+	</div>
+
+	<div class="s-b-text">
+	<a href="#">Aqui podras  aprender facilmente y jugando  para que sea mas chevere y ameno estudiar</a>	
+	</div>
+	</div>
+		
+	<div class="s-box">
+	
+	<div class="s-b-img">
+	
+		<div class="s-type">Encicloplay 1</div>
+	
+		<img src="images/s3.png">
+	</div>
+	
+	<div class="s-b-text">
+	<a href="#">Pagina de proyecto creada desde un principio desde 0</a>	
+	</div>
+	</div>
+	
+	<div class="s-box">
+	
+	<div class="s-b-img">
+		
+
+		<div class="s-type">Lenguajes de programacion</div>
+	
+		<img src="images/s2.png">
+	</div>
+
+	<div class="s-b-text">
+		
+	<a href="#">Pagina creada desde 0 ,sobre los lenguajes de programacion con sus respectivas paginas y cursos</a>	
+	</div>
+	</div>
+	</div>
+	</section>
+	
+	<section id="portfolio">
+
+	<h1 class="p-headind">Portafolio</h1>
+
+	<div class="p-container">
+
+	<div class="p-box">
+
+	<div class="overlay-text">
+	<h1>Javascript</h1>
+	<p>Java</p>	
+	</div>
+
+	<img src="images/w1.jpg">
+		
+	</div>
+
+	<div class="p-box">
+		
+	<div class="overlay-text">
+	<h1>Css</h1>
+	<p>Diseño</p>	
+	</div>
+	
+	<img src="images/w2.jpg">
+	</div>
+	
+	<div class="p-box">
+
+	<div class="overlay-text">
+	<h1>HTML</h1>
+	<p>Codigo</p>	
+	</div>
+	
+	<img src="images/w3.jpg">
+	</div>
+	
+	<div class="p-box">
+
+	<div class="overlay-text">
+	<h1>Front-end</h1>
+	<p>Web Desing</p>	
+	</div>
+	
+	<img src="images/w4.jpg">
+	</div>
+
+	<div class="p-box">
+
+	<div class="overlay-text">
+	<h1>Manual</h1>
+	<p>Usuario</p>	
+	</div>
+		
+	<img src="images/w5.jpg">
+	</div>
+	
+	<div class="p-box">
+	
+	<div class="overlay-text">
+	<h1>Fronted</h1>
+	<p>Web Desing</p>	
+	</div>
+		
+	<img src="images/w6.jpg">
+	</div>
+		
+	</div>
+	</section>
+	
+	<section id="contact-btn">
+	
+	<h1 class="c-b-heading"><font style="border-bottom: 3px solid #AF3669;"> Tienes algún proyecto en mente?</h1>
+	
+		<button >Contactame</button>
+	</section>
+	
+	<section id="contact-form">
+	<form>
+
+	<div class="contact-left">
+	<h1 class="c-l-heading"><font style="border-bottom: 3px solid #AF3669;">Escribeme</font></h1>
+
+	<div class="f-name">
+	<font >Nombre</font>
+	<input type="text" placeholder="Nombre Completo"/>
+	</div>
+	
+	<div class="f-email">
+	<font >Email</font>
+	<input type="email" placeholder="Example@gmail.com"/>
+	</div>
+	</div>
+	
+	<div class="contact-right">
+	
+	<div class="message">
+	<font >Mensage</font>
+	<textarea name="message" rows="5" cols="20" placeholder="Escribe algo..."></textarea>
+	</div>
+	
+	<button>submit</button>
+	</div>
+		
+	</form>
+	
+	</section>
+	
+	<script type="text/javascript" src="js/js.js"></script>
+	<script type="text/javascript">
+	
+		 
+	  $(document).ready(function(){
+	 $('.toggle').click(function(){
+		 $('.toggle').toggleClass('active')
+		 
+		 $('nav ul').toggleClass('active-menu')
+		
+		 
+	 })
+ });
+	
+	</script>
+</body>
+</html>
---
+++ portafolio HV/portafoliophp/manuales/index.html
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="icon" href="imagen/guia-del-usuario.png" >
+	<title>Manual De Usuario</title>
+	<link rel="stylesheet" type="text/css" href="stylo.css">
+</head>
+<body>
+	<section id="portfolio">
+
+	<h1 class="p-headind">MANUAL DE USUARIO Y TECNICO</h1>
+</section>
+
+
+
+
+<table>
+	<thead>
+		<tr>
+			<th><tt>INTRODUCCION!</th><th></th></tt>
+		</tr>
+	</thead>
+
+	<tr>
+		<td><tt>
+
+ Es un documento técnico<br> de un determinado sistema que intenta dar asistencia a sus usuarios.En general,<br><li> Un Manual de Usuario</li><br> debería poder ser entendido por cualquier usuario principiante, como así también serle útil a usuarios avanzados;<br><li> Manual Tecnico</li><br>
+ documento contiene toda la información sobre los recursos utilizados por el proyecto,<br>explicado todo el trabajo que se ha realizado al desarrollar un sistema llevan una descripción muy bien detallada sobre las características físicas y técnicas de cada elemento..</td><td><img src="imagen/guia.jpg"alt="" width="100px" height="100px"></a>
+		<div class="text-black text-center my-3"></td>
+	</tr></tt>
+
+
+
+
+	<table>
+	<thead>
+		<tr>
+			<th><tt>DEFINICION MANUAL DE USUARIO</th><th></th></tt>
+		</tr>
+	</thead>
+
+	<tr>
+		<td><tt>
+
+Una Guía de usuario,<br> también conocida como Manual de usuario, es un documento de comunicación técnica destinado a dar asistencia a las personas que utilizan un sistema en particular.<br> Por lo general, este documento está redactado por un escritor técnico<br> como por ejemplo los programadores del sistema o los directores de proyectos implicados en su desarrollo, o el personal técnico, especialmente en las empresas más pequeñas.</td><td><img src="imagen/s1.jpg"alt="" width="100px" height="100px"></a>
+		<div class="text-black text-center my-3"></td>
+	</tr></tt>
+
+	<table>
+	<thead>
+		<tr>
+			<th><tt>ESTRUCTURA MANUAL DE USUARIO</th><th></th></tt>
+		</tr>
+	</thead>
+
+	<tr>
+		<td><tt>
+
+   Una estructura frecuente de los manuales de usuario<br><li> incluye una introducción al producto en cuestión</li><li> un índice con los contenidos del manual</li> <li>la guía en sí misma, una sección de problemas frecuentes y su forma de solucionarlos</li><li> los datos de contacto y un glosario</li></td><td><img src="imagen/estructura.webp"alt="" width="170px" height="170px"></a>
+		<div class="text-black text-center my-3"></td>
+	</tr></tt>
+
+
+
+
+
+	<table>
+	<thead>
+		<tr>
+			<th><tt>DEFINICION MANUAL TECNICO</th><th></th></tt>
+		</tr>
+	</thead>
+
+	<tr>
+		<td><tt>
+
+Un manual técnico<br> 
+ es aquel que va dirigido a un público con conocimientos técnicos sobre algún área.<br>
+La documentación de proyectos es importante para identificar mas fácilmente los aspectos y características que forman parte de un proyecto.<br></td><td><img src="imagen/s2.jpg"alt="" width="100px" height="100px"></a>
+		<div class="text-black text-center my-3"></td>
+
+
+	</tr></tt>
+	<table>
+	<thead>
+		<tr>
+			<th><tt>ESTRUCTURA MANUAL TECNICO</th><th></th></tt>
+		</tr>
+	</thead>
+
+	<tr>
+		<td><tt>
+
+Un manual técnico<br> 
+ El manual técnico va dirigido a la dirección de IT, al administrador del sistema y a otros desarrolladores de software<br> para que puedan darle mantenimiento en caso que se requiera.<br>
+
+<li>Manual técnico debe contener:</li>
+
+<li>Objetivo y alcances del sistema.</li>
+<li>Manual de Normas, políticas y procedimientos de la organización en las que se basa el sistema para su implementación.</li>
+<li>Descripción de bases de datos y diagramas de relación diseño de reportes y pantallas.</li></td><td><img src="imagen/estructura2.jpg"alt="" width="170px" height="170px"></a>
+		<div class="text-black text-center my-3"></td>
+	</tr></tt>
+
+
+
+
+	
+</body>
+</html>
